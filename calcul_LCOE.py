df2bda06921e181d427f4591e86e682179ecf938
Fily Baptiste <baptiste.fily@student-cs.fr>
2025-03-17 11:56:53 +0100

    LCOE

--- calcul_LCOE.py
+++ calcul_LCOE.py
@@ -1,3 +1,4 @@
+# Description: Calcul du LCOE pour un microgrid
 from Data import *
 
 import csv
@@ -9,13 +10,33 @@ file_name = "Data_project.xlsx"
 wb = openpyxl.load_workbook(file_name, data_only = True)
 sheet = wb.active  
 
+
+
 ##Capex  
 #n_PV = ? # nombre de panneaux solaires installés
 #n_WT = ? # nombre d'éoliennes installées
 #n_batterie = ? # nombre de batteries installées
 #n_hydrogene = ? # nombre de systèmes à hydrogène installés
+#n_generateur = ? # nombre de générateurs diesel installés
 
-Capex_PV = float(sheet["G9"].value) if sheet["G9"].value is not None else 0.0       #en $/kWc ; à clarifier 
-Capex_WT = (float(sheet["K8"].value) if sheet["K8"].value is not None else 0.0) * ((float(sheet["K6"].value) if sheet["K6"].value is not None else 0.0))
+#Capex pour 1 entité de chaque type
+Capex_PV = (float(sheet["G9"].value) if sheet["G9"].value is not None else 0.0) * (float(sheet["G6"].value) if sheet["G6"].value is not None else 0.0)
+Capex_WT = (float(sheet["K8"].value) if sheet["K8"].value is not None else 0.0) * (float(sheet["K6"].value) if sheet["K6"].value is not None else 0.0)
+Capex_batterie = (float(sheet["C7"].value) if sheet["C7"].value is not None else 0.0) * (float(sheet["C10"].value) if sheet["C10"].value is not None else 0.0)
+#Capex_hydro = 
+Capex_generateur = (float(sheet["K8"].value) if sheet["K8"].value is not None else 0.0) * (float(sheet["K8"].value) if sheet["K8"].value is not None else 0.0)
 
-print(Capex_PV)
+
+#Opex pour 1 entité ; On va les créer sous forme de liste avec chaque élément qui correspondra à une année distincte
+Opex_PV = []
+opex_annuel_PV = (float(sheet["G8"].value) if sheet["G8"].value is not None else 0.0) * (float(sheet["G6"].value) if sheet["G6"].value is not None else 0.0)
+Discount_rate = (float(sheet["AA6"].value) if sheet["AA6"].value is not None else 0.0)
+for i in range(25) : 
+    opex_annualise_PV = opex_annuel_PV/(1+Discount_rate)**i
+    Opex_PV.append(opex_annualise_PV)
+
+#print(Opex_PV)
+print(opex_annuel_PV)
+#print(Discount_rate)
+
+
