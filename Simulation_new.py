02651a201bd06984f10af8525445c1da18223e3e
carm0uche <quentin.houvenaghel@student-cs.fr>
2025-03-25 10:50:56 +0100

    simulation_new

---
+++ Simulation_new.py
@@ -0,0 +1,30 @@
+from Classes import *
+from Calculs_new import *
+from Data import *
+from calcul_LCOE import *
+
+
+def simulation_microgrid(params):
+
+    #Simulation avec les paramètres correspondants
+    Simu = calcul_simu(params, True)
+
+    #Calcul shortage (on somme tous les shortages de la liste)
+    Stg = - sum(Simu[1])
+
+    #Calcul du bilan carbone global - 2.7kg de CO2 par litre de diesel brûlé
+    BC = sum(50 + 0.236 * P for P in Simu[0]) * 2.7
+
+    #Calcul de la LCOE avec la fonction de Baptiste
+    LCOE = calcul_LCOE(params)
+
+    return LCOE, BC, Stg
+
+
+
+
+
+
+
+
+
