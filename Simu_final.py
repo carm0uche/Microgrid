810aeb6441c4ace7f94e18d0ae37de6937b75538
carm0uche <quentin.houvenaghel@student-cs.fr>
2025-03-27 10:01:58 +0100

    lcoe dans simu final

--- Simu_final.py
+++ Simu_final.py
@@ -15,7 +15,7 @@ def simulation(paramètres, durée = len(load), données = True, graphique = Fal
     --
     Simulation complète
     --
-    Renvoie : LCOE, Bilan Carbone, Data
+    Renvoie : LCOE, Consommation de fuel, Profile
     '''
 
     def dispatch(i, Batterie_level, H2_level) :
@@ -103,19 +103,73 @@ def simulation(paramètres, durée = len(load), données = True, graphique = Fal
         for j in range(7):
             profile[j].append(current_profile[j])
 
-    # Calcul_LCOE
-    LCOE = None
+    # Calcul LCOE
+    ## LCOE PV ------------------------
+    Capex_PV = Panneau_solaire.capex * Panneau_solaire.power * paramètres[1]
+    Opex_PV = []
+    opex_annuel_PV = Panneau_solaire.power * Panneau_solaire.opex * paramètres[1]
+    for i in range(Lifetime) : 
+        opex_annualise_PV = opex_annuel_PV/(1 + Discount_rate)**i
+        Opex_PV.append(opex_annualise_PV)
 
-    # Bilan Carbone 
-    BC = sum((0.05 * int(P/1000) + 1) + 0.236 * P for P in profile[2]) * 2.7 + 700000 * paramètres[0] + 50 * paramètres[1]
+    ## WT ------------------------
+    Capex_WT = Eolienne.capex * Eolienne.rated_power * paramètres[0]
+    Opex_WT = []
+    opex_annuel_WT = Eolienne.rated_power * Eolienne.opex * paramètres[0]
+    for i in range(Lifetime) : 
+        opex_annualise_WT = opex_annuel_WT/(1+Discount_rate)**i
+        Opex_WT.append(opex_annualise_WT)
+
+    ## Diesel ------------------------
+
+    Capex_Diesel = Generateur_diesel.capex * Generateur_diesel.max_power * paramètres[2]
+    Opex_Diesel = []
+    opex_annuel_Diesel = Generateur_diesel.max_power * Generateur_diesel.opex * paramètres[2]
+    for i in range(Lifetime) : 
+        opex_annualise_Diesel = opex_annuel_Diesel/(1+Discount_rate)**i
+        Opex_Diesel.append(opex_annualise_Diesel)
+
+    ## H2 -------------------------------- //à compléter//
+
+    Capex_H2 = (Stockage_hydrogene.capacity * Stockage_hydrogene.capex_tank / 33.3 + Stockage_hydrogene.charge_pw_max * Stockage_hydrogene.capex_el) * paramètres[4] 
+    Opex_H2 = []
+    opex_annuel_H2 = (Stockage_hydrogene.capacity * Stockage_hydrogene.opex_tank / 33.3 + Stockage_hydrogene.capacity*Stockage_hydrogene.opex_el) * paramètres[4]
+    for i in range(Lifetime) : 
+        opex_annualise_H2 = opex_annuel_H2/(1+Discount_rate)**i
+        Opex_H2.append(opex_annualise_H2)
+
+    ## Batt ------------------------------
+
+    Capex_Batt = Batterie.capex * Batterie.capacity * paramètres[3]
+    Opex_Batt = []
+    opex_annuel_Batt = Batterie.capacity * Batterie.opex * paramètres[3]
+    for i in range(Lifetime) : 
+        opex_annualise_Batt = opex_annuel_Batt/(1+Discount_rate)**i
+        Opex_Batt.append(opex_annualise_Batt)
+
+    ##TOTAL ----------------------------------
+
+    CAPEX = Capex_WT + Capex_PV + Capex_Diesel + Capex_H2 + Capex_Batt
+    OPEX = sum(Opex_WT) + sum(Opex_PV) + sum(Opex_Diesel) + sum(Opex_Batt)
+    TOTAL_EX = CAPEX + OPEX
+    TOTAL_NRJ = sum(profile[0]) + sum(profile[1]) + sum(profile[2])
+    if TOTAL_NRJ == 0.0 :
+        LCOE = 1e9
+    else :
+        LCOE = TOTAL_EX / TOTAL_NRJ    
+
+    # Consommation fuel 
+    CF = sum((0.05 * int(P/1000) + 1) + 0.236 * P for P in profile[2])
 
     if données :
-        rendu = (LCOE, BC, profile)
+        rendu = (LCOE, CF, profile)
     else :
-        rendu = (LCOE,BC)
+        rendu = (LCOE, CF)
         
     if graphique :
         # Affichage graphique
+        print("Graphique à suivre")
+
     return rendu
 
 
